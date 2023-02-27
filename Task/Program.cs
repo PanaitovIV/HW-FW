979c79394a5aa64c84ed562dc66e6f9639426c0a
Ivan Panaitov <PanaitovIV@gmail.com>
2023-02-27 18:09:08 +0300

    Добавляем задачу

---
+++ Task/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Clear();
+
+int LengthCalculation(string[] array, int numChar = 3)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= numChar)
+            count++;
+    }
+    return count;
+}
+
+string[] CreatingNewArray(string[] array, int numChar = 3)
+{
+    string[] finishArray = new string [LengthCalculation(array)];
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= numChar)
+        {
+            finishArray[index] = array[i];
+            index++;
+        }
+    }
+    return finishArray;
+}
+
+void PrintArray(string[] array, string msg)
+{
+    System.Console.WriteLine(msg);
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i]);
+        if (i != array.Length-1)
+        {
+            System.Console.Write(", ");
+        }
+    }
+    System.Console.Write("]");
+    System.Console.WriteLine();
+}
+string[] firstArray = {"hello", "2", "world", ":-)", "1234", "1567", "-2", "Russia", "Denmark", "Kazan"};
+string[] secondArray = CreatingNewArray(firstArray);
+PrintArray(firstArray, "Базовый массив:");
+System.Console.WriteLine();
+PrintArray(secondArray, "Итоговый массив:");
